5f7ad53789f3ed6a0c80cf4915340f16800a8473
Lachs <lachspost@gmail.com>
2025-10-08 20:26:12 +0200

    implement wrapper function for preprocessing and first predict.py functionality

--- predict.py
+++ predict.py
@@ -10,10 +10,33 @@ from preprocessing import *
 
 # ------------------------------------------ Variables ------------------------------
 
+# there is no preprocessing for numerical features yet, so they are added here 
+numerical_features = ['srv_serror_rate',
+    'same_srv_rate',
+    'dst_host_same_srv_rate',
+    'dst_host_srv_diff_host_rate',
+    'dst_host_count',
+    'duration',
+    'src_bytes',
+    'dst_host_diff_srv_rate',
+    'dst_host_srv_serror_rate',
+    'dst_host_serror_rate',
+    'srv_count',
+    'dst_host_srv_rerror_rate',
+    'dst_bytes',
+    'dst_host_srv_count',
+    'serror_rate',
+    'diff_srv_rate',
+    'dst_host_same_src_port_rate',
+    'srv_diff_host_rate',
+    'srv_rerror_rate',
+    'dst_host_rerror_rate',
+    'rerror_rate',
+    'count']
 filename = "KDDTest+.txt"
 MODELS = {"RF": 'model/random_forest_model.pkl'} # Models avaiable for prediction (saved as pkl files)
 # path = "d:/PYTHON/CS_Bootcamp/programs/cs-intrusion-detection-system/data/KDDTest+.txt"
-
+# TODO: add baseline models for prediction 
 
 # ----------------------------------------------
 # demo code 
@@ -33,7 +56,8 @@ if __name__ == "__main__":
     if arguments[1] in MODELS.keys():
         
         # load the model 
-        print(f"- Chosen model: {arguments[1]}") 
+        print("\n----------")
+        print(f"- Load model: {arguments[1]}") 
         loaded_model = pickle.load(open(MODELS[arguments[1]], 'rb'))
     else:
         print(f"- Unknown model. Expects one of {MODELS.keys} as second argument.")
@@ -41,20 +65,28 @@ if __name__ == "__main__":
     
     # check input 2nd and 3rd arguments 
     if len(arguments) == 3:
-        print('\n- Make prediction without evaluation.')
-       
+        #print('\n- Make prediction without evaluation.') --> no y values given 
+        
+        # TODO: provide seperate input for X and y values 
+        # ----------------------------------------------
         # read 2nd argument as path to X data that includes y (for now )
+        print("- Read data...")
         filepath = arguments[2]
-        df_test = read_data_to_df(filepath)
-
+        df_test = read_data_to_df(filepath) 
         # ----------------------------------------------
         # data preprocessing / feature engineering
-        # create a preprocessing wrapper function so we do not have to deal with the details here
-
+        print("- Data preprocessing ...")
+        categorial_features = preprocessing_categories(df_test)
         # ----------------------------------------------
-        # make prediction
-         #y_test_pred = loaded_model.predict(X_test)
-         
+        # select features (must be the same the model was trained on)
+        df_X = df_test[ numerical_features + categorial_features]
+        # ----------------------------------------------
+        # make prediction 
+        print("- Predicting... ")
+        y_prediction = loaded_model.predict(df_X)
+        print(y_prediction)
+        # ----------------------------------------------
+        # TODO: improve the output "prediciton"
 
     elif len(arguments) == 4: # (optional)
         print('\n- Make prediction with evaluation')
--- scripts/preprocessing.py
+++ scripts/preprocessing.py
@@ -4,10 +4,159 @@
 
 import pandas as pd
 import numpy as np
+import os
+
+# ---------------------------------------- variables ----------------------------------------
+
+file_name_train_data = "KDDTrain+.txt"
+file_name_test_tata = "KDDTest+.txt"
 
 VERBOSE = 0 # enable extra print statments with 1, disable with 0
-BINARY_FEATURE_THRESHOLD = 0.99
-BINARY_FEATURE_NEW_CAT = 1 # attention! add check: make sure the first cat is not also 1
+
+# group features regarding their processing steps
+TARGET_FEATURE = "attack_type " # recoded to binary feature (attack vs no attack) 
+NUM_FEATURES = [
+    'srv_serror_rate',
+    'same_srv_rate',
+    'dst_host_same_srv_rate',
+    'dst_host_srv_diff_host_rate',
+    'dst_host_count',
+    'duration',
+    'src_bytes',
+    'dst_host_diff_srv_rate',
+    'dst_host_srv_serror_rate',
+    'dst_host_serror_rate',
+    'srv_count',
+    'dst_host_srv_rerror_rate',
+    'dst_bytes',
+    'dst_host_srv_count',
+    'serror_rate',
+    'diff_srv_rate',
+    'dst_host_same_src_port_rate',
+    'srv_diff_host_rate',
+    'srv_rerror_rate',
+    'dst_host_rerror_rate',
+    'rerror_rate',
+    'count']
+CAT_FEATURES = [
+    'logged_in', 
+    'root_shell', 
+    'is_guest_login', 
+    'land', 
+    'flag', 
+    'difficulty_level', 
+    'protocol_type', 
+    'service']
+RECODE_NUM_TO_BINARY_CAT = ['num_shells',
+                    'urgent',
+                    'num_root',           # --> works with threshold .99
+                    'num_file_creations',
+                    'num_failed_logins',
+                    'su_attempted',
+                    'num_access_files',
+                    'wrong_fragment',     # --> works with threshold .99
+                    ]
+BINARY_FEATURE_THRESHOLD = 0.99     # only used to find categories in the training data
+BINARY_FEATURE_NEW_CAT = 1          # attention! add check: make sure the first cat is not also 1
+RECODE_NUM_TO_THREE_CAT = {
+    'num_compromised': 10, 
+    'hot': 5} 
+
+# ------------------------------------ utility functions ------------------------------------
+
+def preprocessing_categories(data_df):
+    """
+    Wrapper function that includes all preprocessing steps for the respective features. 
+
+    - 1 step: convert categorical variables to categories 
+        --> CAT_FEATURES
+    - 2 step: recode numerical variables to binary categorical 
+        --> RECODE_NUM_TO_BINARY_CAT
+    - 3 step: recode numerical variables to categorical with 3 categories 
+        --> RECODE_NUM_TO_THREE_CAT
+
+    Returns list of all categorical features included in model training. 
+
+    """
+    new_categories = set([]) # all categorical features after preprocessing 
+
+    # --- 1 step: convert categorical variables to categories
+    for feature in CAT_FEATURES:
+        data_df = convert_column_type(data_df, feature, 'category')
+        
+        # add feature to new categories 
+        if is_categorical_dtype(data_df, feature):
+            new_categories.add(feature)
+        else:
+            print(f"Feature {new_feature} not added to categories, check preprocessing.")
+
+    # --- 2 step: recode numerical variables to binary categorical
+    for feature in RECODE_NUM_TO_BINARY_CAT:
+        new_feature = feature + "_cat"
+        # note: do not use the threshold here, it was used in the training data to define the categories.
+        # from EDA: assume 0 is the most frequent values and recode all other values to 1 
+        data_df[new_feature] = [x if x == 0 else 1 for x in data_df[feature]]
+
+        # convert to categorical 
+        data_df = convert_column_type(data_df, new_feature, 'category')
+        
+        # add feature to new categories 
+        if is_categorical_dtype(data_df, new_feature):
+            new_categories.add(new_feature)
+        else:
+            print(f"Feature {new_feature} not added to categories, check preprocessing.")
+
+    # --- 3 step: recode numerical variables to categorical with 3 categories
+    category_labels = ["none", "low", "high"] 
+    for feature, boundary in RECODE_NUM_TO_THREE_CAT.items():
+        
+        categories = get_conditions(data_df, feature, boundary)
+        new_feature = feature + "_cat"
+        recode_to_categories(data_df, new_feature, categories, category_labels)
+        
+        # add feature to new categories 
+        if is_categorical_dtype(data_df, new_feature):
+            new_categories.add(new_feature)
+        else:
+            print(f"Feature {new_feature} not added to categories, check preprocessing.")
+
+    return list(new_categories)
+
+
+def read_data_to_df(path_to_file:str) -> pd.DataFrame | None:
+    """
+    Read data from .txt or .csf file and return a pandas DF
+    if the file can be found at 'path_to_file', otherwise return None.
+    
+    Data must have the same format as the data set the models were traiend on:
+    "KDDTrain+.txt" (43 columns)
+
+    Args:
+        path_to_file (str): 
+
+    Returns:
+        pd.DataFrame | None: DF with column names as below. 
+    """
+    # TODO: catch exceptions
+
+    column_names = ["duration", "protocol_type", "service","flag", "src_bytes", "dst_bytes", "land",
+               "wrong_fragment", "urgent", "hot", "num_failed_logins", "logged_in", "num_compromised",
+               "root_shell", "su_attempted", "num_root", "num_file_creations", "num_shells", "num_access_files", 
+               "num_outbound_cmds", "is_host_login", "is_guest_login", "count", "srv_count", "serror_rate",
+               "srv_serror_rate","rerror_rate", "srv_rerror_rate", "same_srv_rate", "diff_srv_rate", 
+               "srv_diff_host_rate","dst_host_count", "dst_host_srv_count", "dst_host_same_srv_rate", 
+               "dst_host_diff_srv_rate", "dst_host_same_src_port_rate", "dst_host_srv_diff_host_rate",
+               "dst_host_serror_rate", "dst_host_srv_serror_rate", "dst_host_rerror_rate",
+               "dst_host_srv_rerror_rate", "attack_type", "difficulty_level"]
+
+    if not os.path.exists(path_to_file):
+        print(f"Cannot find '{path_to_file}'")
+        return []
+    
+    else:
+        return pd.read_csv(path_to_file,  names=column_names)
+    
+
 
 def convert_column_type(df_data: pd.DataFrame, columns: list | str, to_type) -> pd.DataFrame:
     """ Convert data types of column(s) in a dataframe.
@@ -128,7 +277,7 @@ def recode_to_categories(data_df: pd.DataFrame,
     return  
 
 
-def get_conditions(df_data: pd.Dataframe, feature: str, boundary:int):
+def get_conditions(df_data: pd.DataFrame, feature: str, boundary:int):
     """
     Create a mask to recode a numerical freature of the DF 
     to a categorical feature with 3 categories.
